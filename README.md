c7d75d2d084cbbf80294e3c69ad95656cafccbec
dezhizhang <1541609448@qq.com>
2022-10-05 09:22:52 +0800

    feat:数组的排序

--- README.md
+++ README.md
@@ -1270,3 +1270,52 @@ public class Array04 {
   }
 }
 ```
+###  数组的排序
+```java
+package com.xiaozhi.pkg.arrays;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Array05 {
+  public static void main(String[] args) {
+    Book[] books = new Book[4];
+    books[0] = new Book("红楼梦~", 100);
+    books[1] = new Book("金瓶梅~", 90);
+    books[2] = new Book("青年文摘~", 5);
+    books[3] = new Book("java从入门到放弃", 300);
+    Arrays.sort(books, new Comparator<Book>() {
+      @Override
+      public int compare(Book o1, Book o2) {
+        return (int) (o2.getPrice() - o1.getPrice());
+      }
+    });
+
+    for (int i = 0; i < books.length; i++) {
+      System.out.println(books[i].getName().toString());
+    }
+
+  }
+}
+
+class Book {
+  private String name;
+  private double price;
+
+  Book(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+}
+
+```
--- src/com/xiaozhi/pkg/arrays/Array05.java
+++ src/com/xiaozhi/pkg/arrays/Array05.java
@@ -2,11 +2,44 @@ package com.xiaozhi.pkg.arrays;
 
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Array05 {
   public static void main(String[] args) {
-//    List asList = Arrays.asList(2,3,4,5,6,1);
-//    System.out.println(asList);
+    Book[] books = new Book[4];
+    books[0] = new Book("红楼梦~", 100);
+    books[1] = new Book("金瓶梅~", 90);
+    books[2] = new Book("青年文摘~", 5);
+    books[3] = new Book("java从入门到放弃", 300);
+    Arrays.sort(books, new Comparator<Book>() {
+      @Override
+      public int compare(Book o1, Book o2) {
+        return (int) (o2.getPrice() - o1.getPrice());
+      }
+    });
+
+    for (int i = 0; i < books.length; i++) {
+      System.out.println(books[i].getName().toString());
+    }
 
   }
+}
+
+class Book {
+  private String name;
+  private double price;
+
+  Book(String name, double price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public double getPrice() {
+    return this.price;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
 }
