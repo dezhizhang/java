def3a032fba04210b9ad610bd865ae9dd717f7f3
dezhizhang <1541609448@qq.com>
2022-10-05 09:55:26 +0800

    feat:数组的应用

--- src/com/xiaozhi/pkg/arrays/Array07.java
+++ src/com/xiaozhi/pkg/arrays/Array07.java
@@ -4,10 +4,16 @@ import java.util.Arrays;
 
 public class Array07 {
   public static void main(String[] args) {
-    int[] src = {1, 2, 3};
+//    int[] src = {1, 2, 3};
+//    int[] dest = new int[3];
+//
+//    System.arraycopy(src, 0, dest, 0, 3);
+//    System.out.println(Arrays.toString(dest));
+
+    int[] src = {1,2,3};
     int[] dest = new int[3];
 
-    System.arraycopy(src, 0, dest, 0, 3);
+    System.arraycopy(src,0,dest,0,src.length);
     System.out.println(Arrays.toString(dest));
   }
 }
--- src/com/xiaozhi/pkg/arrays/Array08.java
+++ src/com/xiaozhi/pkg/arrays/Array08.java
@@ -1,2 +1,8 @@
-package com.xiaozhi.pkg.arrays;public class Array08 {
+package com.xiaozhi.pkg.arrays;
+
+public class Array08 {
+  public static void main(String[] args) {
+//    System.out.println(System.currentTimeMillis());
+    System.out.println(System.currentTimeMillis());
+  }
 }
