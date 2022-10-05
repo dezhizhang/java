63a28b4da9c05e1c2a1a7594bfb397ffe4b95296
dezhizhang <1541609448@qq.com>
2022-10-05 15:43:51 +0800

    feat:Iterator的使用

---
+++ src/com/xiaozhi/pkg/collection/Collection2.java
@@ -0,0 +1,40 @@
+package com.xiaozhi.pkg.collection;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Collection2 {
+  public static void main(String[] args) {
+    Collection col = new ArrayList();
+    col.add(new Book("三国演义", "罗轴中", 10.1));
+    col.add(new Book("小李飞刀", "古龙", 5.1));
+    col.add(new Book("红楼梦", "蓝雪匠", 34.6));
+
+    Iterator iterator = col.iterator();
+    while (iterator.hasNext()) {
+      //返回下一个元素
+      Object next = iterator.next();
+      System.out.println(next);
+    }
+  }
+}
+
+
+class Book {
+  private String name;
+  private String author;
+  private double price;
+
+  public Book(String name, String author, double price) {
+    this.name = name;
+    this.author = author;
+    this.price = price;
+  }
+
+  @Override
+  public String toString() {
+    return "书名：" + this.name + "作者：" + this.author + "价格:" + this.price;
+  }
+}
