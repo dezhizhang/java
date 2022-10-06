b6ae0105b1bfe451a167aaa398593aed65e7acf3
dezhizhang <1541609448@qq.com>
2022-10-06 19:53:59 +0800

    feat:io

--- src/com/xiaozhi/pkg/generic/Generic09.java
+++ src/com/xiaozhi/pkg/generic/Generic09.java
@@ -1,7 +1,54 @@
 package com.xiaozhi.pkg.generic;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 public class Generic09 {
   public static void main(String[] args) {
 
   }
+  @Test
+  public void TestList() {
+    Dao<User> dao = new Dao<>();
+    dao.save("001",new User());
+    dao.save("002",new User());
+    dao.save("003",new User());
+
+    System.out.println(dao.list());
+  }
+}
+
+class User{
+
+}
+
+class Dao<T> {
+  private Map<String, T> map = new HashMap<>();
+
+  public T get(String id) {
+    return map.get(id);
+  }
+
+  public void update(String id, T entity) {
+    map.put(id, entity);
+  }
+
+  public List<T> list() {
+    List<T> list = new LinkedList<>();
+    Set<String> keySet = map.keySet();
+    for (String key : keySet) {
+      list.add(get(key));
+    }
+    return list;
+  }
+
+  public void delete(String id) {
+    map.remove(id);
+  }
+
+  public void save(String id, T entity) {
+    map.put(id, entity);
+  }
+
 }
---
+++ src/com/xiaozhi/pkg/io/Io01.java
@@ -0,0 +1,39 @@
+package com.xiaozhi.pkg.io;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Io01 {
+  public static void main(String[] args) {
+    created();
+  }
+
+  public static void created() {
+    String filePath = "./test.txt";
+    File file = new File(filePath);
+
+    try {
+      file.createNewFile();
+      System.out.println("文件创建成功");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+//  @Test
+  public void created1() {
+    File file = new File("./text1");
+    String fileName = "news.txt";
+    File file1 = new File(file,fileName);
+
+    try {
+      file1.createNewFile();
+      System.out.println("文件创建成功");
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
+
