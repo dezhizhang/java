d09cef635efb0b98b941d4540911803cb04ccb4f
dezhizhang <1541609448@qq.com>
2022-10-06 11:20:54 +0800

    feat:update

--- src/com/xiaozhi/pkg/generic/Generic02.java
+++ src/com/xiaozhi/pkg/generic/Generic02.java
@@ -1,7 +1,63 @@
 package com.xiaozhi.pkg.generic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 public class Generic02 {
   public static void main(String[] args) {
+    List<Student> list = new ArrayList<Student>();
+    list.add(new Student("小明",22));
+    list.add(new Student("小黄",33));
+    list.add(new Student("大黄",44));
 
+    for (Student student:list) {
+      System.out.println(student.toString());
+    }
+
+    Iterator<Student> integer = list.iterator();
+    while (integer.hasNext()) {
+      Student next = integer.next();
+      System.out.println(next.toString());
+    }
+
+    HashMap<String,Student> hashMap = new HashMap<String,Student>();
+    hashMap.put("小明",new Student("小明",22));
+    hashMap.put("小黄",new Student("小黄",33));
+    hashMap.put("大黄",new Student("大黄",44));
+
+   System.out.println(hashMap);
+  }
+}
+
+class Student{
+  private String name;
+  private int age;
+  public Student(String name,int age) {
+    super();
+    this.name = name;
+    this.age = age;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  @Override
+  public String toString() {
+    return this.name + this.age;
   }
 }
