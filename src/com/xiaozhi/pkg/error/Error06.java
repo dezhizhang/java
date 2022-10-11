067f7c7838d62845c556497b22448ff2f67f47b0
dezhizhang <1541609448@qq.com>
2022-10-12 05:28:35 +0800

    feat:update

---
+++ src/com/xiaozhi/pkg/error/Error06.java
@@ -0,0 +1,17 @@
+package com.xiaozhi.pkg.error;
+
+public class Error06 {
+  public static void main(String[] args) {
+
+  }
+}
+
+class ReturnExceptionDemo {
+  static void methodA() {
+    try{
+
+    } finally {
+      System.out.println("用A");
+    }
+  }
+}
