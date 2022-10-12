0550a19b95362c9f26614ba6f51b776f02db2e5c
dezhizhang <1541609448@qq.com>
2022-10-12 22:41:36 +0800

    feat:反射

--- src/com/xiaozhi/pkg/reflection/Reflection01.java
+++ src/com/xiaozhi/pkg/reflection/Reflection01.java
@@ -1,18 +1,26 @@
 package com.xiaozhi.pkg.reflection;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Properties;
 
 @SuppressWarnings({"all"})
 public class Reflection01 {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
     Properties properties = new Properties();
     properties.load(new FileInputStream("./refect.properties"));
     String classFullPath = properties.getProperty("classfullpath").toString();
-    String method = properties.getProperty("method").toString();
+    String methodName = properties.getProperty("method").toString();
 
     System.out.println(classFullPath);
-    System.out.println(method);
+    System.out.println(methodName);
+    Class clazz = Class.forName(classFullPath);
+    Object o = (Cat) clazz.newInstance();
+    Method method = clazz.getMethod(methodName);
+    method.invoke(o);
+
   }
 }
