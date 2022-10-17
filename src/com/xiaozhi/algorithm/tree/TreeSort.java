add5d6190069265d65dab34d196610571052b14c
dezhizhang <1541609448@qq.com>
2022-10-18 05:24:37 +0800

    feat:堆排序

--- src/com/xiaozhi/algorithm/tree/TreeSort.java
+++ src/com/xiaozhi/algorithm/tree/TreeSort.java
@@ -1,16 +1,48 @@
 package com.xiaozhi.algorithm.tree;
 
+import java.util.Arrays;
+
 public class TreeSort {
   public static void main(String[] args) {
-
+    int arr[] = {4, 6, 8, 5, 9};
+    heapSort(arr);
   }
+
   //堆排序方法
   public static void heapSort(int arr[]) {
-
+    int temp = 0;
+    // 调整为大顶堆
+    for (int i = arr.length / 2 - 1; i >= 0; i--) {
+      adjustHeap(arr, i, arr.length);
+    }
+    // 调换数据
+    for(int j=arr.length - 1;j > 0;j--) {
+      temp = arr[j];
+      arr[j] = arr[0];
+      arr[0] = temp;
+      adjustHeap(arr,0,j);
+    }
+    System.out.println("调整后的数据" + Arrays.toString(arr));
   }
-  // 将一个数组调整成一个大顶堆
-  public static void adjustHeap (int arr[], int i,int length) {
 
+  // 将一个数组调整成一个大顶堆
+  public static void adjustHeap(int arr[], int i, int length) {
+    int temp = arr[i];
+    for (int k = i * 2 + 1; k < length; k = k * 2 + 1) {
+      if (k + 1 < length && arr[k] < arr[k + 1]) {
+        k++; // k指向右子节点
+      }
+      if (arr[k] > temp) {
+        // 如果子节点大于父节点
+        // 把较大的值赋给当前节点
+        arr[i] = arr[k];
+        i = k; // 指向k继续循环比较
+      } else {
+        break;
+      }
+    }
+    // 当for循环完后，已经装i为父结点的最大值放在最顶(局部)
+    arr[i] = temp;
   }
 }
 
