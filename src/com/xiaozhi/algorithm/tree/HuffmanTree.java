c2ac2f4a6915ec141d0a2b9a5bfc20117ab7e5de
dezhizhang <1541609448@qq.com>
2022-10-18 06:32:52 +0800

    feat:赫夫曼树

--- src/com/xiaozhi/algorithm/tree/HuffmanTree.java
+++ src/com/xiaozhi/algorithm/tree/HuffmanTree.java
@@ -1,8 +1,46 @@
 package com.xiaozhi.algorithm.tree;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class HuffmanTree {
   public static void main(String[] args) {
+    int arr[] = {13, 7, 8, 3, 29, 6, 1};
+    Node node = createHuffmanTree(arr);
+    System.out.println("node" + node);
+  }
 
+  public static Node createHuffmanTree(int[] arr) {
+    //将node放入集合中
+    List<Node> nodes = new ArrayList<>();
+    for (int value : arr) {
+      nodes.add(new Node(value));
+    }
+
+    while (nodes.size() > 1) {
+
+      Collections.sort(nodes);
+      // 取出根节点权值最小的两颗二叉树
+      //(1) 取出权值最小的节点
+      Node left = nodes.get(0);
+      //(2) 取出权值第二小的的节点
+      Node right = nodes.get(1);
+
+      //(3)构建成一颗新的二叉树
+      Node parent = new Node(left.value + right.value);
+      parent.left = left;
+      parent.right = right;
+
+      //(4)从ArrayList删除处理过的二叉树
+      nodes.remove(left);
+      nodes.remove(right);
+
+      //(5)将parent加入到nodes
+      nodes.add(parent);
+    }
+    // 返回赫夫曼树的节点
+    return nodes.get(0);
   }
 }
 
