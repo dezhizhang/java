4fff3741c680597d2fbe73ffa1488cffd937c342
dezhizhang <1541609448@qq.com>
2022-10-17 22:56:38 +0800

    feat:update

--- src/com/xiaozhi/algorithm/tree/ArrayToTree.java
+++ src/com/xiaozhi/algorithm/tree/ArrayToTree.java
@@ -6,6 +6,105 @@ public class ArrayToTree {
   }
 }
 
+class BinaryTree1 {
+  private HeroNode1 root;
+  private HeroNode1 pre;
+
+  public void setRoot(HeroNode1 root) {
+    this.root = root;
+  }
+
+  public void threadedNodes(HeroNode1 node1) {
+    if (node1 == null) {
+      return;
+    }
+    threadedNodes(node1.getLeft());
+
+    //
+    if (node1.getLeft() == null) {
+      node1.setLeft(pre);
+      node1.setLeftType(1);
+    }
+
+    // 处理后继节点
+    if (pre != null && pre.getRight() == null) {
+      // 让前驱节点的右指针指向当前节点
+      pre.setRight(node1);
+      pre.setRightType(1);
+    }
+    // 第处理一个节点后，让当前节点是下一个结点的前驱节点
+    pre = node1;
+
+    threadedNodes(node1.getRight());
+  }
+
+  public void preOrder() {
+    if (this.root == null) {
+      System.out.println("二叉树为空不能遍历");
+      return;
+    }
+    this.root.preOrder();
+  }
+
+  // 中序遍历
+  public void infixOrder() {
+    if (this.root == null) {
+      System.out.println("二叉树为空不能遍历");
+      return;
+    }
+
+    this.root.infixOrder();
+  }
+
+  // 后序遍历
+  public void postOrder() {
+    if (this.root == null) {
+      System.out.println("二叉树为空不能遍历");
+      return;
+    }
+    this.root.postOrder();
+  }
+
+  // 前序遍历
+  public HeroNode1 preOrderSearch(int no) {
+    if (this.root == null) {
+      System.out.println("二叉树为空不能遍历");
+      return null;
+    }
+    return this.root.preOrderSearch(no);
+  }
+
+  // 中序遍历
+  public HeroNode1 infixOrderSearch(int no) {
+    if (this.root == null) {
+      System.out.println("二叉树为空不能遍历");
+      return null;
+    }
+
+    return this.root.infixOrderSearch(no);
+  }
+
+  // 后序遍历
+  public HeroNode1 postOrderSearch(int no) {
+    if (this.root == null) {
+      System.out.println("二叉树为空不能遍历");
+      return null;
+    }
+    return this.root.postOrderSearch(no);
+  }
+
+  public void deleteNode(int no) {
+    if (this.root == null) {
+      System.out.println("二叉树为空不能遍历");
+    }
+    if (root.getNo() == no) {
+      root = null;
+      return;
+    }
+    this.root.deleteNode(no);
+  }
+}
+
 
 class HeroNode1 {
   private int no;
@@ -13,10 +112,29 @@ class HeroNode1 {
   private HeroNode1 left;
   private HeroNode1 right;
 
+  private int leftType;
+  private int rightType;
+
   public HeroNode1(int no, String name) {
     super();
     this.no = no;
     this.name = name;
+  }
+
+  public void setLeftType(int leftType) {
+    this.leftType = leftType;
+  }
+
+  public int getLeftType() {
+    return leftType;
+  }
+
+  public void setRightType(int rightType) {
+    this.rightType = rightType;
+  }
+
+  public int getRightType() {
+    return rightType;
   }
 
   public void setNo(int no) {
