e1302b7e0f832012d57a5043fb24dd31530027fb
dezhizhang <1541609448@qq.com>
2022-10-18 04:56:19 +0800

    feat:update

---
+++ src/com/xiaozhi/algorithm/tree/TreeSort.java
@@ -0,0 +1,17 @@
+package com.xiaozhi.algorithm.tree;
+
+public class TreeSort {
+  public static void main(String[] args) {
+
+  }
+  //堆排序方法
+  public static void heapSort(int arr[]) {
+
+  }
+  // 将一个数组调整成一个大顶堆
+  public static void adjustHeap (int arr[], int i,int length) {
+
+  }
+}
+
+
