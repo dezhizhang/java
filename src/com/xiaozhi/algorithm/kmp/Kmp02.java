23f517a0b133517221b4cca69a880aaae9b076d2
dezhizhang <1541609448@qq.com>
2022-10-22 08:16:28 +0800

    feat:kmp算法

--- src/com/xiaozhi/algorithm/kmp/Kmp02.java
+++ src/com/xiaozhi/algorithm/kmp/Kmp02.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 
 public class Kmp02 {
   public static void main(String[] args) {
-    String str1 = "BBC ABCDAB ABCDABCDADDE";
-    String str2 = "BBC";
+    String str1 = "BBC ABCDAB ABCDABDCDADDE";
+    String str2 = "ABCDABD";
 
-    int[] next = kmpNext("AAAB");
-    int index = kmpSearch(str1,str2,next);
+    int[] next = kmpNext("ABCDABD");
+    int index = kmpSearch(str1, str2, next);
     System.out.println("index=" + index);
   }
 
   public static int kmpSearch(String str1, String str2, int[] next) {
     for (int i = 0, j = 0; i < str1.length(); i++) {
+      while (j > 0 && str1.charAt(i) != str2.charAt(j)) {
+        j = next[j - 1];
+      }
       if (str1.charAt(i) == str2.charAt(j)) {
         j++;
       }
