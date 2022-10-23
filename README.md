ba636c58def5674cc620a37b9f37896c27a6c6dc
dezhizhang <1541609448@qq.com>
2022-10-23 20:52:57 +0800

    feat:update

--- README.md
+++ README.md
@@ -4594,6 +4594,28 @@ public class ArrayDuplicates {
   }
 }
 ```
+### 相等中间索引
+```java
+public class CenterIndex {
+  public static void main(String[] args) {
+    System.out.println(pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+  }
+
+  public static int pivotIndex(int[] nums) {
+    int sum = Arrays.stream(nums).sum();
+    int total = 0;
+    for (int i = 0; i < nums.length; i++) {
+      total += nums[i];
+      if (total == sum) {
+        return i;
+      }
+      sum = sum - nums[i];
+    }
+    return -1;
+  }
+}
+
+```
 
 
 
--- src/com/xiaozhi/leetcode/centerIndex/CenterIndex.java
+++ src/com/xiaozhi/leetcode/centerIndex/CenterIndex.java
@@ -1,2 +1,22 @@
-package com.xiaozhi.leetcode.centerIndex;public class CenterIndex {
+package com.xiaozhi.leetcode.centerIndex;
+
+import java.util.Arrays;
+
+public class CenterIndex {
+  public static void main(String[] args) {
+    System.out.println(pivotIndex(new int[]{1, 7, 3, 6, 5, 6}));
+  }
+
+  public static int pivotIndex(int[] nums) {
+    int sum = Arrays.stream(nums).sum();
+    int total = 0;
+    for (int i = 0; i < nums.length; i++) {
+      total += nums[i];
+      if (total == sum) {
+        return i;
+      }
+      sum = sum - nums[i];
+    }
+    return -1;
+  }
 }
