60b055ff8b53112aa38daf23f0b856e7c9f7ada4
dezhizhang <1541609448@qq.com>
2022-10-24 08:16:50 +0800

    feat:对像的序列化

---
+++ src/com/xiaozhi/desigin/prototype/DeepCloneableTarget.java
@@ -0,0 +1,7 @@
+package com.xiaozhi.desigin.prototype;
+
+import java.io.Serializable;
+
+public class DeepClone implements Serializable,Cloneable {
+  private static final
+}
---
+++ src/com/xiaozhi/desigin/prototype/DeepProtoType.java
@@ -0,0 +1,2 @@
+package com.xiaozhi.desigin.prototype;public class DeepProtoType {
+}
