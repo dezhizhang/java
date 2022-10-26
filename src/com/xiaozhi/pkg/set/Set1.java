c1b376c1ca7ee2fdf5f08d9af87f001280fe960d
dezhizhang <1541609448@qq.com>
2022-10-27 06:46:44 +0800

    feat:update

--- src/com/xiaozhi/pkg/set/Set1.java
+++ src/com/xiaozhi/pkg/set/Set1.java
@@ -6,17 +6,16 @@ import java.util.Iterator;
 public class Set1 {
   public static void main(String[] args) {
 
-
     HashSet hashSet = new HashSet();
-    hashSet.add("tom");
-    hashSet.add("key");
-    hashSet.add("change");
-    hashSet.add(null);
+    hashSet.add("red");
+    hashSet.add("yellow");
+    hashSet.add("orange");
+    hashSet.add("pink");
 
-    Iterator iterator = hashSet.iterator();
-    while (iterator.hasNext()) {
-      Object next = iterator.next();
-      System.out.println(next);
-    }
+   Iterator iterator = hashSet.iterator();
+   while (iterator.hasNext()) {
+     Object next = iterator.next();
+     System.out.println(next);
+   }
   }
 }
