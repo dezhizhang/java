20c84876d4143a9bbb4e4d89a39af5aa8ff314c8
dezhizhang <1541609448@qq.com>
2022-10-28 06:21:25 +0800

    feat:map

---
+++ src/com/xiaozhi/pkg/map/Map01.java
@@ -0,0 +1,17 @@
+package com.xiaozhi.pkg.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Map01 {
+  public static void main(String[] args) {
+    HashMap<Object, Object> map = new HashMap<>();
+    map.put("no1", "刘德华");
+    map.put("no2", "周华建");
+
+
+    System.out.println("map=" + map);
+  }
+}
+
+
