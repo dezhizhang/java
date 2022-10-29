c0886dc4685f15be87eb793fc83c64e6a3012d1e
dezhizhang <1541609448@qq.com>
2022-10-29 09:50:13 +0800

    feat:update

--- src/com/xiaozhi/pkg/map/Map06.java
+++ src/com/xiaozhi/pkg/map/Map06.java
@@ -1,13 +1,39 @@
 package com.xiaozhi.pkg.map;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class Map06 {
   public static void main(String[] args) {
     Map map = new HashMap();
 
+    map.put(1, new Employee("jack", 1, 20, 3000));
+    map.put(1, new Employee("tom", 2, 20, 20000));
+    map.put(1, new Employee("mack", 3, 30, 18000));
+
+
+    Set keySet = map.keySet();
+
+    for (Object key : keySet) {
+      Employee employee = (Employee) map.get(key);
+
+      if (employee.getSalary() > 1800) {
+        System.out.println(employee);
+      }
+
+    }
+
+
+    Set set = map.entrySet();
+    Iterator iterator = set.iterator();
+
+    while (iterator.hasNext()) {
+      Map.Entry entry = (Map.Entry) iterator.next();
+
+      Employee employee = (Employee) entry.getValue();
+      if (employee.getSalary() > 1800) {
+        System.out.println(employee);
+      }
+    }
 
   }
 }
@@ -16,13 +42,24 @@ public class Map06 {
 class Employee {
   private String name;
   private int age;
+
+  private int id;
   private double salary;
 
-  public Employee(String name, int age, double salary) {
+  public Employee(String name, int id, int age, double salary) {
+    this.id = id;
     this.name = name;
     this.age = age;
     this.salary = salary;
 
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public int getId() {
+    return id;
   }
 
   public double getSalary() {
@@ -63,7 +100,7 @@ class Employee {
 
   @Override
   public String toString() {
-    return super.toString();
+    return "Employee{" + "name='" + name + '\'' + ", age=" + age + ", id=" + id + ", salary=" + salary + '}';
   }
 }
 
