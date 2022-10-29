97f51c7e6b713efee227ab3451245e14f8d23898
dezhizhang <1541609448@qq.com>
2022-10-29 15:32:19 +0800

    feat:update

---
+++ src/com/xiaozhi/game/draw/Draw.java
@@ -0,0 +1,18 @@
+package com.xiaozhi.game.draw;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Draw extends  JFrame {
+  public static void main(String[] args) {
+
+  }
+}
+
+class MyPanel extends JPanel {
+  @Override
+  public void paint(Graphics g) {
+    super.paint(g);
+
+  }
+}
