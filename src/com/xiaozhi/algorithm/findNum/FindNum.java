a664a18ecf715cd37b3f8ee787caba1e7c739e28
dezhizhang <1541609448@qq.com>
2022-10-30 23:10:34 +0800

    feat:update

--- src/com/xiaozhi/algorithm/findNum/FindNum.java
+++ src/com/xiaozhi/algorithm/findNum/FindNum.java
@@ -1,28 +1,29 @@
 package com.xiaozhi.algorithm.findNum;
 
+import java.util.Arrays;
+
 public class FindNum {
   public static void main(String[] args) {
-      int[] arr = new int[]{1,2,3,4,5,6,9,10};
-      System.out.println(find(arr,6));
+    int[] arr = new int[]{1, 2, 3, 4, 5, 6, 9, 10};
+    System.out.println(Arrays.toString(find(arr, 6)));
   }
 
-  public static boolean find(int[] arr,int num) {
-    if(arr == null || arr.length == 0) {
-      return false;
+  public static int[] find(int[] arr, int num) {
+    if (arr == null || arr.length == 0) {
+      return new int[0];
     }
-
-    int L = 0;
-    int R = arr.length - 1;
-    while (L < R) {
-      int mid = (R - L) / 2 + L;
+    int left = 0;
+    int right = arr.length - 1;
+    while (left < right) {
+      int mid = (right - left) / 2 + left;
       if (arr[mid] == num) {
-        return true;
-      }else if(arr[mid] < num) {
-        L = mid + 1;
-      }else {
-        R = mid - 1;
+        return new int[]{mid};
+      } else if (arr[mid] < num) {
+        left = mid + 1;
+      } else if(arr[mid] > num) {
+        right = mid - 1;
       }
     }
-    return false;
+    return new int[0];
   }
 }
