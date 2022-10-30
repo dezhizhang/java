df6a27913f8437ff33ac745f1b81f97d92eba9ba
dezhizhang <1541609448@qq.com>
2022-10-30 20:06:16 +0800

    feat:广度优先

--- src/com/xiaozhi/algorithm/treeNode/TreeNode.java
+++ src/com/xiaozhi/algorithm/treeNode/TreeNode.java
@@ -1,5 +1,10 @@
 package com.xiaozhi.algorithm.treeNode;
 
+import com.xiaozhi.algorithm.linkList.LinkList;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
   public static void main(String[] args) {
     Node node7 = new Node(7, null, null);
@@ -11,27 +16,51 @@ public class TreeNode {
     Node node1 = new Node(1, node2, node3);
 
 
-    System.out.println(minDepth(node1));
+    System.out.println(minDepth1(node1));
   }
 
   public static int minDepth(Node root) {
-    if(root == null) {
+    if (root == null) {
       return 0;
     }
-    if(root.left == null && root.right == null) {
+    if (root.left == null && root.right == null) {
       return 1;
     }
 
     int min = Integer.MAX_VALUE;
-    if(root.left != null) {
-      min = Math.min(minDepth(root.left),min);
+    if (root.left != null) {
+      min = Math.min(minDepth(root.left), min);
     }
 
-    if(root.right != null) {
-      min = Math.min(minDepth(root.right),min);
+    if (root.right != null) {
+      min = Math.min(minDepth(root.right), min);
     }
     return min + 1;
 
+  }
+
+  public static int minDepth1(Node root) {
+    if (root == null) {
+      return 0;
+    }
+    Queue<Node> queue = new LinkedList<>();
+    root.setDeep(1);
+    queue.offer(root);
+    while (!queue.isEmpty()) {
+      Node node = queue.poll();
+      if (node.left == null && node.right == null) {
+        return node.deep;
+      }
+      if (node.left != null) {
+        node.left.setDeep(node.deep + 1);
+        queue.offer(node.left);
+      }
+      if (node.right != null) {
+        node.right.setDeep(node.deep + 1);
+        queue.offer(node.right);
+      }
+    }
+    return 0;
   }
 
 }
@@ -41,10 +70,16 @@ class Node {
   public Node left;
   public Node right;
 
+  public int deep;
+
   public Node(int val, Node left, Node right) {
     this.val = val;
     this.left = left;
     this.right = right;
+  }
+
+  public void setDeep(int deep) {
+    this.deep = deep;
   }
 
   @Override
