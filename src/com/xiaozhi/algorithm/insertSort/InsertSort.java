d8b83d706d6fb87e17583a5d0ed00b11e3cec2a6
dezhizhang <1541609448@qq.com>
2022-11-01 05:33:59 +0800

    feat:update

--- src/com/xiaozhi/algorithm/insertSort/InsertSort.java
+++ src/com/xiaozhi/algorithm/insertSort/InsertSort.java
@@ -12,17 +12,16 @@ public class InsertSort {
   }
 
   public static void insertSort(int[] arr) {
+    int insertValue = 0;
+    int insertIndex = 0;
     for (int i = 1; i < arr.length; i++) {
-      int insertValue = arr[i];
-      int insertIndex = i - 1;
-
+      insertValue = arr[i];
+      insertIndex = i - 1;
       while (insertIndex >= 0 && insertValue < arr[insertIndex]) {
         arr[insertIndex + 1] = arr[insertIndex];
         insertIndex--;
       }
-
       arr[insertIndex + 1] = insertValue;
-
     }
   }
 
