bab4d305ac77f48ad0cb928227b46c1ef7b9a655
dezhizhang <1541609448@qq.com>
2022-11-01 06:35:57 +0800

    feat:update

--- src/com/xiaozhi/algorithm/shellSort/ShellSort01.java
+++ src/com/xiaozhi/algorithm/shellSort/ShellSort01.java
@@ -2,24 +2,24 @@ package com.xiaozhi.algorithm.shellSort;
 
 import java.util.Arrays;
 
-public class ShellSort {
+public class ShellSort01 {
   public static void main(String[] args) {
     int[] arr = {8,9,1,7,2,3,5,4,6,0};
     shellSort(arr);
 
     System.out.println(Arrays.toString(arr));
   }
-
   public static void shellSort(int[] arr) {
-    int temp = 0;
     for(int gap = arr.length / 2; gap > 0;gap /=2) {
       for(int i = gap;i < arr.length;i++) {
-        for(int j = i - gap;j >= 0;j -= gap) {
-          if(arr[j] > arr[j + gap]) {
-            temp = arr[j];
-            arr[j] = arr[j + gap];
-            arr[j + gap] = temp;
+        int j = i;
+        int temp = arr[j];
+        if(arr[j] < arr[j - gap]) {
+          while (j - gap >= 0 && temp < arr[j - gap]) {
+            arr[j] = arr[j - gap];
+            j-= gap;
           }
+          arr[j] = temp;
         }
       }
     }
