6c89ef035240f31e4234ce63cd1472913b05ae73
dezhizhang <1541609448@qq.com>
2022-11-10 23:00:46 +0800

    feat:update

---
+++ src/com/xiaozhi/leetcode/array/Array01.java
@@ -0,0 +1,14 @@
+package com.xiaozhi.leetcode.array;
+
+import java.util.ArrayList;
+
+public class Array01 {
+  public static void main(String[] args) {
+    ArrayList<Integer> arr = new ArrayList<>();
+    for(int i=0;i < 3;i++) {
+      arr.add(i + 1);
+    }
+    System.out.println(arr);
+
+  }
+}
