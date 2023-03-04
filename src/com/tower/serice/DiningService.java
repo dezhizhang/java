842988c69632168356722419b50643e30616ae33
dezhizhang <1541609448@qq.com>
2023-03-04 22:27:52 +0800

    feat:更餐桌信息

--- src/com/tower/serice/DiningService.java
+++ src/com/tower/serice/DiningService.java
@@ -6,13 +6,25 @@ import com.tower.domain.Dining;
 import java.util.List;
 
 public class DiningService {
-  private DiningDao  diningDao = new DiningDao();
+  private DiningDao diningDao = new DiningDao();
 
   // 返回所有餐桌所有信息
   public List<Dining> list() {
     String sql = "select id,status from dining";
     List<Dining> dinings = diningDao.queryMulti(sql, Dining.class);
     return dinings;
+  }
+
+  // 通过id获取餐桌信息
+  public Dining getDiningById(int id) {
+    String sql = "select * from dining where id = ?";
+    return diningDao.querySingle(sql, Dining.class, id);
+  }
+
+  public boolean orderDining(int id, String name, String phone) {
+    String sql = "update dining set status = 1,name = ?,phone = ? where id = ?";
+    int update = diningDao.update(sql, name, phone);
+    return update > 0;
   }
 
 }
--- src/com/tower/view/ManinView.java
+++ src/com/tower/view/ManinView.java
@@ -35,31 +35,6 @@ public class ManinView {
   // 显示主菜单
   public void mainMenu() {
     Scanner scanner = new Scanner(System.in);
-    while (loop) {
-      System.out.println("===============满汉楼============");
-      System.out.println("\t\t 1登录满汉楼");
-      System.out.println("\t\t 2退出满汉楼");
 
-      System.out.println("请输入你的选择：");
-      key = scanner.next();
-
-      switch (key) {
-        case "1":
-          System.out.println("输入员工号:");
-          userId = scanner.nextInt();
-          System.out.println("请输入密码：");
-          password = scanner.next();
-          Employee employee =  employService.getEmployeeByIdAndName(userId,password);
-
-          break;
-        case "2":
-          loop = false;
-          break;
-        default:
-          System.out.println("输入有误请重新输入");
-      }
-
-
-    }
   }
 }
