883089e702a9da723e691e44dda48d72440390ea
dezhizhang <1541609448@qq.com>
2023-03-04 22:15:30 +0800

    feat:满汉楼

--- src/com/tower/view/ManinView.java
+++ src/com/tower/view/ManinView.java
@@ -50,47 +50,7 @@ public class ManinView {
           System.out.println("请输入密码：");
           password = scanner.next();
           Employee employee =  employService.getEmployeeByIdAndName(userId,password);
-          if(employee == null) {
-            System.out.println("登录失败");
-            break;
-          }
 
-          while (loop) {
-            System.out.println("============满汉楼二级菜单========");
-            System.out.println("\t\t 1 显示餐桌状态");
-            System.out.println("\t\t 2 预定餐桌");
-            System.out.println("\t\t 3 显示所有菜品");
-            System.out.println("\t\t 4 点餐服务");
-            System.out.println("\t\t 5 查看帐单");
-            System.out.println("\t\t 6 结帐");
-            System.out.println("\t\t 9 通出系统");
-            switch (key) {
-              case "1":
-                showDiningStatus();
-                break;
-              case "2":
-                System.out.println("预定餐桌");
-                break;
-              case "3":
-                System.out.println("显示所有菜品");
-                break;
-              case "4":
-                System.out.println("点餐服务");
-                break;
-              case "5":
-                System.out.println("查看帐单");
-                break;
-              case "6":
-                System.out.println("结帐");
-              case "9":
-                System.out.println("通出系统");
-                loop = false;
-                break;
-              default:
-                System.out.println("请输新登录");
-
-            }
-          }
           break;
         case "2":
           loop = false;
