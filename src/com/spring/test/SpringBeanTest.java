9f7439142d6fd22555204fe220a14dfad97248eb
dezhizhang <1541609448@qq.com>
2023-03-05 15:30:39 +0800

    feat:类加载

--- src/com/spring/test/SpringBeanTest.java
+++ src/com/spring/test/SpringBeanTest.java
@@ -5,14 +5,30 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.File;
+
 public class SpringBeanTest {
 
   @Test
   public void  getMonster() {
-    ApplicationContext ico = new ClassPathXmlApplicationContext("beans.xml");
+//    ApplicationContext ico = new ClassPathXmlApplicationContext("beans.xml");
+//
+//    Monster monster01 = ico.getBean("monster01",Monster.class);
+//    System.out.println(monster01);
 
-    Monster monster01 = (Monster) ico.getBean("monster01");
+    ApplicationContext ioc = new ClassPathXmlApplicationContext("beans.xml");
+
+    Monster monster01 = ioc.getBean("monster01",Monster.class);
+
     System.out.println(monster01);
+
+  }
+
+  //类加载路径
+  @Test
+  public void  classPath() {
+    File file = new File(this.getClass().getResource("/").getPath());
+    System.out.println("file=" + file);
 
   }
 }
