3ca6e2467de75f29997d6e65fedf706d933d9a3c
dezhizhang <1541609448@qq.com>
2023-03-11 15:34:32 +0800

    feat:切面异常

--- src/com/spring/aop1/AnimalAspect.java
+++ src/com/spring/aop1/AnimalAspect.java
@@ -2,6 +2,8 @@ package com.spring.aop1;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -11,8 +13,20 @@ import java.util.Arrays;
 @Component
 public class AnimalAspect {
   @Before(value = "execution(public float com.spring.aop1.Dog.getSum(float, float))")
-  public static void f1(JoinPoint joinPoint) {
+  public void f1(JoinPoint joinPoint) {
     Signature signature = joinPoint.getSignature();
     System.out.println("方法执行前-方法名" + signature.getName() + Arrays.asList(joinPoint.getArgs()));
+  }
+
+  // 返回通知
+  @AfterReturning(value = "execution(public float com.spring.aop1.Dog.getSum(float,float))")
+  public void  f2(JoinPoint joinPoint) {
+    Signature signature = joinPoint.getSignature();
+    System.out.println("返回结果" + signature.getDeclaringTypeName());
+  }
+  @AfterThrowing(value = "execution(public float com.spring.aop1.Dog.getSum(float,float))")
+  public void f3(JoinPoint joinPoint) {
+    Signature signature = joinPoint.getSignature();
+    System.out.println("方法出现异常" + signature.getDeclaringType());
   }
 }
