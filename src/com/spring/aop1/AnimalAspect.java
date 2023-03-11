7c0ef80751579c5d705ff926e9fa650215028da3
dezhizhang <1541609448@qq.com>
2023-03-11 15:48:27 +0800

    feat:aop注意事项

--- src/com/spring/aop1/AnimalAspect.java
+++ src/com/spring/aop1/AnimalAspect.java
@@ -10,28 +10,28 @@ import java.util.Arrays;
 @Aspect
 @Component
 public class AnimalAspect {
-  @Before(value = "execution(public float com.spring.aop1.Dog.getSum(float, float))")
-  public void f1(JoinPoint joinPoint) {
+  @Before(value = "execution(public float com.spring.aop1.Dog.*(..))")
+  public void showBeginLog(JoinPoint joinPoint) {
     Signature signature = joinPoint.getSignature();
     System.out.println("方法执行前-方法名" + signature.getName() + Arrays.asList(joinPoint.getArgs()));
   }
 
   // 返回通知
-  @AfterReturning(value = "execution(public float com.spring.aop1.Dog.getSum(float,float))")
-  public void f2(JoinPoint joinPoint) {
+  @AfterReturning(value = "execution(public float com.spring.aop1.Dog.*(..))")
+  public void showSuccessEndLog(JoinPoint joinPoint) {
     Signature signature = joinPoint.getSignature();
     System.out.println("返回结果" + signature.getDeclaringTypeName());
   }
 
-  @AfterThrowing(value = "execution(public float com.spring.aop1.Dog.getSum(float,float))")
-  public void f3(JoinPoint joinPoint) {
+  @AfterThrowing(value = "execution(public float com.spring.aop1.Dog.*(..))")
+  public void showExceptLog(JoinPoint joinPoint) {
     Signature signature = joinPoint.getSignature();
     System.out.println("方法出现异常" + signature.getDeclaringType());
   }
 
   // 最终通知
-  @After(value = "execution(public float  com.spring.aop1.Dog.getSum(float,float))")
-  public void f4(JoinPoint joinPoint) {
+  @After(value = "execution(public float  com.spring.aop1.Dog.*(..))")
+  public void showFinallyEndLog(JoinPoint joinPoint) {
     Signature signature = joinPoint.getSignature();
     System.out.println("最终通知");
   }
